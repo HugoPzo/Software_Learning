53b8f1288d4068e1e71b3183cf52991c472af6a0
hugo_pzz <hugo.ortz.04@gmail.com>
2024-09-09 15:28:51 -0600

    Adding Tkinter Scale

---
+++ PythonBroCode/GUI/gui_radio.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+
+
+window = Tk()
+
+window.config(bg="black")
+
+# radio button = similar to checkbox, but you can only select one from a group
+img = PhotoImage(file="IMG/img.png")
+logo = PhotoImage(file="IMG/Logo.png")
+msi = PhotoImage(file="IMG/msi.png")
+
+images = [img, logo, msi] # List of images, assign an image to each radio button
+
+foods = ["Pizza", "Hamburger", "Tacos"] # Radio Button of food
+
+x = IntVar()
+
+def click_radio():
+    for i in range(len(foods)):
+        if x.get() == i:
+            print(f"You order {foods[i]}")
+
+for index in range(len(foods)):
+    radio_button = Radiobutton(window,
+                               text=foods[index], # adds text to buttons
+                               # * IMPORTANT *
+                               variable=x, # groups radio buttons together if they share the same variable
+                               value=index, # assign each radiobutton a different value
+                               # ************
+                               pady=10,
+                               font=("Impact", 10),
+                               image=images[index], # Assign each radio button an image
+                               compound="left", # Set image at left
+                               indicatoron=False, # Eliminate 'radio' buttons
+                               width=500, # Width of buttons
+                               command=click_radio, # Action after clicking a radio button
+                               fg="#00FF00",
+                               bg="black",
+                               activebackground="blue",
+                               activeforeground="#CCC",
+                               )
+
+
+
+    radio_button.pack(anchor=E) # Set button at 'E'ast of the window
+
+
+window.mainloop()
---
+++ PythonBroCode/GUI/gui_scale.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+# scale of numbers
+
+def temperature():
+    print(f"The temperature is {scale.get()} degrees...") # Print value set in scale
+
+window = Tk()
+window.config(bg="black")
+
+image_1 = PhotoImage(file="IMG/Logo.png")
+image_2 = PhotoImage(file="IMG/img.png")
+
+label_1 = Label(window, image=image_1)
+label_2 = Label(window, image=image_2)
+
+scale = Scale(window,
+              from_=450, to=-50,
+              length=400,
+              tickinterval=10, # Adds numeric indicators for value
+              orient=VERTICAL, # Scale's orientation
+              resolution= 5, # Increment of slider
+              troughcolor="#00AEEA",
+              showvalue=True, # Show value of scale
+              sliderrelief="flat",
+              bg="black",fg="#00FF00",
+              )
+
+
+
+scale.set(((scale["from"]-scale["to"])/2)+scale["to"]) # Set current position of slider
+                                    # +scale["to"] just in case is greater than zero
+
+button = Button(window,
+                text="Submit",
+                command=temperature,)
+
+label_1.pack()
+scale.pack()
+label_2.pack()
+button.pack()
+window.mainloop()
