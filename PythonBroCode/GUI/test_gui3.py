c03bc853e9f522007a17f2b28ab37267df9a1b16
HugosLaptop <hugo.ortz.04@gmail.com>
2024-09-13 17:03:22 -0600

    Add Calculator

---
+++ PythonBroCode/GUI/test_gui3.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from turtledemo.nim import randomrow
+
+window = Tk()
+window["bg"] = "black"
+
+num_buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+for num in num_buttons:
+    if num_buttons[]
+    button_num = Button(window, width=10, text=num)
+    button_num.pack(side=LEFT)
+
+window.mainloop()
