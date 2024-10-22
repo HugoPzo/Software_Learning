71824eb6b6ee852f670ab20635d17b44265f6e52
hugo_pzz <hugo.ortz.04@gmail.com>
2024-10-21 22:49:22 -0600

    Calculator

--- PythonBroCode/GUI/GUI_Programs/Calculator/OwnCalculator/calculator.py
+++ PythonBroCode/GUI/GUI_Programs/Calculator/OwnCalculator/calculator.py
@@ -1,35 +1,47 @@
 from tkinter import *
 
+TOTAL_WIDTH = 300
+TOTAL_HEIGHT = 537
 
-TOTAL_WIDTH = 40
-TOTAL_HEIGHT = 5
 
 def create_gui():
     window = Tk()
     window.title("Hugo's Calculator")
-    window.geometry("500x500")
+    window.geometry(f"{TOTAL_WIDTH}x{TOTAL_HEIGHT}")
     window.config(bg="#000")
     logoImage = PhotoImage(file="logoCalc.png")
     window.iconphoto(True, logoImage)
 
     return window
 
+
 WINDOW = create_gui()
 
-def create_frame(window):
-    frame = Frame(window)
-    frame.grid(row=1, column=0)
-    
+
+def create_label_frame(window):
+    frame = Frame(window, width=TOTAL_WIDTH, height=100, relief="ridge", border="5")
+    frame.pack(side=TOP)
+    return frame
 
 
 def button_click(num):
+    frame = create_label_frame(WINDOW)
+    frame.after(1)
 
     labelText = StringVar()
     labelText.set(num)
-    label = Label(WINDOW, textvariable=labelText, fg="#0F0", bg="#000")
-    WINDOW.after(1)
-    label.grid(row=0, column=0)
+
+    label = Label(frame, textvariable=labelText, fg="#0F0", bg="#000", width=TOTAL_WIDTH, height="100")
+
+    label.pack(side=TOP)
+
     print(num)
+
+
+def create_frame(window):
+    frame = Frame(window, relief="ridge", border="3")
+    frame.pack(side=TOP)
+    return frame
 
 
 def create_buttons(frame):
@@ -42,22 +54,28 @@ def create_buttons(frame):
         column = 0
         for columns in rows:
             #                              Assign the value before it pack it
-            button = Button(frame, text=columns, width=WIDTH, height=HEIGHT, command=lambda i=columns:button_click(i))
+            button = Button(frame, text=columns, width=WIDTH, height=HEIGHT, fg="#0F0", bg="#000",
+                            command=lambda i=columns: button_click(i))
             if columns != "=":
-                button.grid(frame, row=row, column=column)
+                button.grid(row=row, column=column)
             else:
-                button.grid(columnspan=TOTAL_WIDTH)
+                button.grid(row=row, columnspan=4)
                 button.config(width=40)
+
             column += 1
-            
+
         row += 1
 
     return button
 
+
 def main():
+    create_label_frame(WINDOW)
     frame = create_frame(WINDOW)
-    button = create_buttons(frame)
+    create_buttons(frame)
+
     WINDOW.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- PythonBroCode/GUI/GUI_Programs/Calculator/OwnCalculator/prueba.py
+++ PythonBroCode/GUI/GUI_Programs/Calculator/OwnCalculator/prueba.py
@@ -1,13 +1,13 @@
 from tkinter import *
 
 
-TOTAL_WIDTH = 40
-TOTAL_HEIGHT = 5
+TOTAL_WIDTH = 300
+TOTAL_HEIGHT = 528
 
 def create_gui():
     window = Tk()
     window.title("Hugo's Calculator")
-    window.geometry("500x500")
+    window.geometry(f"{TOTAL_WIDTH}x{TOTAL_HEIGHT}")
     window.config(bg="#000")
     logoImage = PhotoImage(file="logoCalc.png")
     window.iconphoto(True, logoImage)
@@ -16,25 +16,33 @@ def create_gui():
 
 WINDOW = create_gui()
 
-def create_frame(window):
-    frame = Frame(window)
+
+def create_label_frame(window):
+    frame = Frame(window, relief="ridge", border="5", )
+    # Make global 'variable'
+    global labelText
+    labelText = StringVar()
+    label = Label(frame, textvariable=labelText, fg="#0F0", bg="#000", width="40", height="5")
+    label.pack()
     frame.pack(side=TOP)
-    
+    frame.after(1)
 
 
 def button_click(num):
-
-    labelText = StringVar()
-    labelText.set(num)
-    label = Label(WINDOW, textvariable=labelText, fg="#0F0", bg="#000")
-    WINDOW.after(1)
-    label.pack(side=TOP)
     print(num)
+    labelText.set(num)
+
+
+
+def create_frame(window):
+    frame = Frame(window, relief="ridge", border="3")
+    frame.pack(side=TOP)
+    return frame
 
 
 def create_buttons(frame):
-    WIDTH = 9
-    HEIGHT = 5
+    BUTTON_WIDTH = 9
+    BUTTON_HEIGHT = 5
     buttonsText = [[7, 8, 9, "/"], [4, 5, 6, "*"], [1, 2, 3, "-"], [".", 0, "C", "+"], ["="]]
 
     row = 0
@@ -42,7 +50,8 @@ def create_buttons(frame):
         column = 0
         for columns in rows:
             #                              Assign the value before it pack it
-            button = Button(frame, text=columns, width=WIDTH, height=HEIGHT, command=lambda i=columns:button_click(i))
+            button = Button(frame, text=columns, width=BUTTON_WIDTH, height=BUTTON_HEIGHT, fg="#0F0", bg="#000",
+                            command=lambda i=columns:button_click(i))
             if columns != "=":
                 button.grid(row=row, column=column)
             else:
@@ -56,8 +65,11 @@ def create_buttons(frame):
     return button
 
 def main():
+    create_label_frame(WINDOW)
     frame = create_frame(WINDOW)
     create_buttons(frame)
+
+
     WINDOW.mainloop()
 
 if __name__ == "__main__":
